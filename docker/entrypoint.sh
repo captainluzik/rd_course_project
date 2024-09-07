5d8274710a9adbae771f0be95aab25932c1d0e70
DL <dmitriy.luzanovsky@gmail.com>
2024-09-07 16:55:44 +0300

    Finalized

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,17 +4,21 @@ set -e
 cd /app
 export PYTHONPATH=$(pwd)
 
-# Выполните миграции
 until alembic upgrade head; do
     echo "Waiting for database migrations to complete..."
     sleep 2
 done
 
-# Запустите начальную загрузку
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+
+until curl -sSf http://0.0.0.0:8000/api/v1/cve/ping; do
+    echo "Waiting for API to become available..."
+    sleep 2
+done
+
 until python app/utils/initial_load.py; do
     echo "Waiting for initial load to complete..."
     sleep 2
 done
 
-# Запустите сервер
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+wait
