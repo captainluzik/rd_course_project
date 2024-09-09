4112c7be33a3c342b34e849bf5014e8f08df7195
DL <dmitriy.luzanovsky@gmail.com>
2024-09-09 18:05:08 +0300

    Updated scheduler

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -12,12 +12,16 @@ scheduler = AsyncIOScheduler(jobstores=jobstores)
 executor = ProcessPoolExecutor()
 
 
+def sync_update_load():
+    asyncio.run(update_load())
+
+
 async def run_update_load():
     loop = asyncio.get_event_loop()
-    await loop.run_in_executor(executor, update_load)
+    await loop.run_in_executor(executor, sync_update_load)
 
 
-@scheduler.scheduled_job('interval', seconds=120)
+@scheduler.scheduled_job('interval', seconds=60)
 async def job():
     await run_update_load()
     print("Job executed")
